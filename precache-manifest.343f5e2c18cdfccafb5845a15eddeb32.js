d5ce8cdc8794857b4269420f30ee6d78307ccc5d
Travis <ga86yam@mytum.de>
2019-10-05 18:09:40 +0200

    Updates

---
+++ precache-manifest.343f5e2c18cdfccafb5845a15eddeb32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f5d98602471b307b2a334712fafc66",
+    "url": "/travistang/GermanHelper-v2/index.html"
+  },
+  {
+    "revision": "5d8d73be339e236dfb7f",
+    "url": "/travistang/GermanHelper-v2/static/css/2.f0405d8a.chunk.css"
+  },
+  {
+    "revision": "19750914ad3da1b37976",
+    "url": "/travistang/GermanHelper-v2/static/css/main.467c12e5.chunk.css"
+  },
+  {
+    "revision": "5d8d73be339e236dfb7f",
+    "url": "/travistang/GermanHelper-v2/static/js/2.df53e8ef.chunk.js"
+  },
+  {
+    "revision": "19750914ad3da1b37976",
+    "url": "/travistang/GermanHelper-v2/static/js/main.b6fe57b3.chunk.js"
+  },
+  {
+    "revision": "0bdadffb5142a439cd12",
+    "url": "/travistang/GermanHelper-v2/static/js/runtime~main.7c31904c.js"
+  }
+]);
