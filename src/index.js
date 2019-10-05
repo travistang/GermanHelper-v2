4e8bf04d079f3680c7269cc2ad5ee3189222283b
Travis <ga86yam@mytum.de>
2019-10-05 18:42:33 +0200

    Trigger scrolling on window load to minimize the address bar

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+window.addEventListener("load",function() {
+	// Set a timeout...
+	setTimeout(function(){
+		// Hide the address bar!
+		window.scrollTo(0, 1);
+	}, 0);
+});
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
